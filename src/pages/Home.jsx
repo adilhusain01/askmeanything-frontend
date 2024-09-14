504b3c052c664388c40f1488c685101fea341269
Adil husain <husainadil202@gmail.com>
2024-09-14 08:11:11 +0530

    ..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,9 @@ const Home = () => {
     <main className="w-full h-screen">
       <Header />
       <section className="grid grid-cols-12">
-        <div className="col-span-4"></div>
+        <div className="col-span-4">
+          <h1>Making Dating Exciting</h1>
+        </div>
         <div className="col-span-4"></div>
         <div className="col-span-4 p-[2.5rem] flex flex-col items-center justify-center gap-[1rem] w-full">
           <Suspense fallback={<div>Loading...</div>}>
